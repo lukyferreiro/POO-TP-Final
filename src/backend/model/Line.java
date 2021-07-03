9b3c6f982e719022a003cd80f81999669e7e1a4c
benjamindelasoie <67805605+benjamindelasoie@users.noreply.github.com>
2021-07-03 15:22:03 -0300

    Update Line.java
    Comportamiento para Linea

--- src/backend/model/Line.java
+++ src/backend/model/Line.java
@@ -1,10 +1,14 @@
 package backend.model;
 
+import javafx.scene.canvas.GraphicsContext;
+
+import java.util.Arrays;
 import java.util.List;
 
 public class Line extends Figure{
 
-    private final Point start, end;
+    private final Point start;
+    private final Point end;
 
     public Line(Point start, Point end){
         this.start = start;
@@ -12,28 +16,29 @@ public class Line extends Figure{
     }
 
     @Override
-    boolean pointBelongs(Point point) {
-        return false;
-    }
-
-    @Override
-    List<Point> getPoints() {
-        return null;
-    }
-
-    @Override
-    public void move(double moveInX, double moveInY) {
-
-    }
-    
-    @Override
     public void draw(GraphicsContext gc) {
         gc.strokeLine(start.getX(), start.getY(),end.getX(), end.getY());
     }
 
+    @Override
+    public boolean isEnclosedBy(Point tl, Point br) {
+        Point topLeft = new Point(Math.min(start.getX(),end.getX()), Math.max(start.getY(), end.getY()));
+        Point bottomRight = new Point(Math.max(start.getX(),end.getX()), Math.min(start.getY(), end.getY()));
+        return new Rectangle(topLeft,bottomRight).isEnclosedBy(tl, br);
+    }
+
+    @Override
+    public boolean pointBelongs(Point point) {
+
+    }
+
+    @Override
+    protected List<Point> getPoints() {
+        return Arrays.asList(start, end);
+    }
 
     @Override
     public String toString() {
-        return String.format("Linea [%.2f , %.2f]", start, end);
+        return String.format("Línea [ %s, %s ]", start, end);
     }
 }
